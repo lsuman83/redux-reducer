fcb1c5e1329b86141797865b025d00219ddd42f8
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-02-17 15:53:09 -0500

    commit tree 68a51d20081491666fa3b683977d4fe5eee6787a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+
+   
+    switch (action.type) {
+
+        case 'ADD_FRIEND': {
+            state.friends.push(action.friend)
+            return {friends: state.friends}
+        }
+        case 'REMOVE_FRIEND': {
+            let foundFriend = state.friends.findIndex(friend => friend.id === action.id)
+
+            return {friends: [...state.friends.slice(0,foundFriend), ...state.friends.slice(foundFriend+1)]}
+        }
+        default:
+            return state;
+    }
 }
